63f4ea37b5a052a155ee3b1512895a4f0a223215
Sriranjanisalla <135413210+Sriranjanisalla@users.noreply.github.com>
2023-06-09 10:36:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-add_my_fruit = streamlit.text_input('What fruit would you like add','Jaclfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like add','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 
